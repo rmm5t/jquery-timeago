dd707da5d3a38a9da747e93b0c7c80d3c94d8e05
Ryan McGeary <ryanongit@mcgeary.org>
2008-07-18 15:58:26 -0400

    More unit tests added

---
+++ test.js
@@ -0,0 +1,16 @@
+var zeropad = function (num) {
+  return ((num < 10) ? '0' : '') + num;
+};
+var iso8601 = function (date) {
+  return date.getUTCFullYear()
+    + "-" + zeropad(date.getUTCMonth()+1)
+    + "-" + zeropad(date.getUTCDate())
+    + "T" + zeropad(date.getUTCHours())
+    + ":" + zeropad(date.getUTCMinutes())
+    + ":" + zeropad(date.getUTCSeconds()) + "Z";
+};
+
+jQuery(document).ready(function($) {
+  $('abbr[class*=loaded]').attr("title", iso8601(new Date()));
+  $('abbr[class*=modified]').attr("title", iso8601(new Date(document.lastModified)));
+});
