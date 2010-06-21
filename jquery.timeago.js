e8e3072d77c2ea6a315075dcde6529588e358a89
Ryan McGeary <ryanongit@mcgeary.org>
2010-06-21 15:09:54 -0400

    Added support for milliseconds in ISO 8601 timestamps [Closes #13]

--- jquery.timeago.js
+++ jquery.timeago.js
@@ -84,6 +84,7 @@
     },
     parse: function(iso8601) {
       var s = $.trim(iso8601);
+      s = s.replace(/\.\d\d\d/,""); // remove milliseconds
       s = s.replace(/-/,"/").replace(/-/,"/");
       s = s.replace(/T/," ").replace(/Z/," UTC");
       s = s.replace(/([\+-]\d\d)\:?(\d\d)/," $1$2"); // -04:00 -> -0400
