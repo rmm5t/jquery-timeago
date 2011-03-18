c2f3d8aaf9977de39ee4b54bce7d1b1b5d0c25ce
aeggli <aeggli@gmail.com>
2011-03-18 16:14:54 +0800

    add word separator setting for i18n, most Asian languages uses no word separator.

--- jquery.timeago.js
+++ jquery.timeago.js
@@ -45,6 +45,7 @@
         months: "%d months",
         year: "about a year",
         years: "%d years",
+        wordSep: " ",
         numbers: []
       }
     },
@@ -84,7 +85,7 @@
         years < 2 && substitute($l.year, 1) ||
         substitute($l.years, Math.floor(years));
 
-      return $.trim([prefix, words, suffix].join(" "));
+      return $.trim([prefix, words, suffix].join($l.wordSep));
     },
     parse: function(iso8601) {
       var s = $.trim(iso8601);
