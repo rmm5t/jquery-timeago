86f2b892e9538522edda9c0a73a6c6b9041f7056
Daniël W. Crompton <daniel.crompton@gmail.com>
2012-06-13 12:28:27 +0200

    Added Dutch locale

--- locales/jquery.timeago.nl.js
+++ locales/jquery.timeago.nl.js
@@ -1,16 +1,20 @@
-// Dutch
+// English (Template)
 jQuery.timeago.settings.strings = {
-   suffixAgo: "geleden",
-   suffixFromNow: "vanaf nu",
-   seconds: "iets minder dan een minuut",
-   minute: "ongeveer een minuut",
-   minutes: "%d minuten",
-   hour: "ongeveer een uur",
-   hours: "ongeveer %d uur",
-   day: "een dag",
-   days: "%d dagen",
-   month: "ongeveer een maand",
-   months: "%d maanden",
-   year: "ongeveer een jaar",
-   years: "%d jaar"
+  prefixAgo: null,
+  prefixFromNow: "",
+  suffixAgo: "geleden",
+  suffixFromNow: "van nu",
+  seconds: "minder dan een minuut",
+  minute: "ongeveer een minute",
+  minutes: "%d minuten",
+  hour: "ongeveer een uur",
+  hours: "ongeveer %d uur",
+  day: "een dag",
+  days: "%d dagen",
+  month: "ongeveer een maand",
+  months: "%d maanden",
+  year: "ongeveer een jaar",
+  years: "%d jaar",
+  wordSeparator: " ",
+  numbers: []
 };
