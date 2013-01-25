4ae611046edcf15348a12e0b3159e7415fae8a06
Stephen Mizell <smizell@gmail.com>
2013-01-25 07:52:05 -0600

    Fix issues with automatic refreshing

--- jquery.timeago.js
+++ jquery.timeago.js
@@ -153,13 +153,15 @@
 
   function prepareData(element) {
     element = $(element);
-    var datetime = $t.datetime(element);
 
     if (!element.data("timeago")) {
+      datetime = $t.datetime(element);
       var text = $.trim(element.text());
       if (text.length > 0 && !($t.isTime(element) && element.attr("title"))) {
         element.attr("title", text);
       }
+    } else {
+      datetime = element.data('timeago').datetime
     }
 
     if (!isNaN(datetime)) {
