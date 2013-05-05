6b2314815d21b326f776efc411b40dc5f827fd6b
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2013-05-05 05:21:27 +0300

    Update jquery.timeago.ar.js

--- locales/jquery.timeago.ar.js
+++ locales/jquery.timeago.ar.js
@@ -1,6 +1,11 @@
+(function() {
+  function numpf(n, a) {
+    return a[plural=n==0 ? 0 : n==1 ? 1 : n==2 ? 2 : n%100>=3 && n%100<=10 ? 3 : n%100>=11 ? 4 : 5];
+  }
+
 jQuery.timeago.settings.strings = {
     prefixAgo: "منذ",
-    prefixFromNow: "يتبقى",
+    prefixFromNow: "بعد",
     suffixAgo: null,
     suffixFromNow: null, // null OR "من الآن"
     second: function(value) { return numpf(value, [
