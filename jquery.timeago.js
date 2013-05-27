5644cc9ff0b511230619a15e4e417615229c7a51
Takashi CHIBA <takashi@wondershake.com>
2013-05-27 15:40:28 +0900

    add mobile safari support(conform to ios8601)

--- jquery.timeago.js
+++ jquery.timeago.js
@@ -104,7 +104,7 @@
     parse: function(iso8601) {
       var s = $.trim(iso8601);
       s = s.replace(/\.\d+/,""); // remove milliseconds
-      s = s.replace(/-/,"/").replace(/-/,"/");
+      s = s.replace(/\//,"-").replace(/\//,"-");
       s = s.replace(/T/," ").replace(/Z/," UTC");
       s = s.replace(/([\+\-]\d\d)\:?(\d\d)/," $1$2"); // -04:00 -> -0400
       return new Date(s);
