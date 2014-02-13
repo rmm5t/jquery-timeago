80687da084b969ff89ecfe6a774e5cd328290d22
vipero07 <yodaissmart@gmail.com>
2014-02-13 17:46:06 -0500

    Fix for secondLength
    secondLength was being calculated incorrectly after the previous change, it should be correct now.

--- jquery.timeago.js
+++ jquery.timeago.js
@@ -126,7 +126,7 @@
       }
 
       var seconds = Math.abs(distanceMillis) / 1000,
-          secondLength = Math.floor(seconds/10)+1,
+          secondLength = Math.floor(Math.log(seconds) / Math.log(10)) + 1,
           words = typeof makeString[secondLength] != "undefined" ? makeString[secondLength](seconds, $l, distanceMillis) : makeString[8](seconds, $l, distanceMillis);
 
       var separator = $l.wordSeparator || "";
