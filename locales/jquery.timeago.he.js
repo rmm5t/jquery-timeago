120a06339d726fbae1eadaddf8024e78cbabf65d
hillai <hillaib@walla.com>
2014-07-26 13:19:40 +0300

    Improved the Hebrew translation
    There's a special Hebrew form for two hours/days/months/years. Also, removed two useless lines.

--- locales/jquery.timeago.he.js
+++ locales/jquery.timeago.he.js
@@ -2,17 +2,15 @@
 jQuery.timeago.settings.strings = {
   prefixAgo: "לפני",
   prefixFromNow: "עוד",
-  suffixAgo: "",
-  suffixFromNow: "",
   seconds: "פחות מדקה",
   minute: "דקה",
   minutes: "%d דקות",
   hour: "שעה",
-  hours: "%d שעות",
+  hours: function(number){return (number==2) ? "שעתיים" : "%d שעות"},
   day: "יום",
-  days: "%d ימים",
+  days: function(number){return (number==2) ? "יומיים" : "%d ימים"},
   month: "חודש",
-  months: "%d חודשים",
+  months: function(number){return (number==2) ? "חודשיים" : "%d חודשים"},
   year: "שנה",
-  years: "%d שנים"
+  years: function(number){return (number==2) ? "שנתיים" : "%d שנים"}
 };
