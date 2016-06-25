9c6dc7b99e76ff52f50ff8410b672edd91a82e30
Oliver Klee <typo3-coding@oliverklee.de>
2016-06-25 20:18:18 +0200

    [TASK] use strict in all scripts except for the locales
    This will help in catching bugs connected to no-quite-clean programming.
    
    The "use strict" lines are placed so that production code of projects
    using this library will not be automatically set to strict mode.

--- contrib/timeago-koext.js
+++ contrib/timeago-koext.js
@@ -1,11 +1,13 @@
 ko.bindingHandlers.timeago = {
     init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        'use strict';
         var value = valueAccessor();
         var valueUnwrapped = ko.unwrap(value);
         element.title = moment(valueUnwrapped).toISOString();
         $(element).timeago();
     },
     update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        'use strict';
         var value = valueAccessor();
         var valueUnwrapped = ko.unwrap(value);
         element.title = moment(valueUnwrapped).toISOString();
