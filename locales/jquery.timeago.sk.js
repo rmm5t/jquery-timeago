4f370928ebe9a3046bef40538e3a5750b89405c9
Radek Hrančík <radek.hrancik@gmail.com>
2017-04-17 17:09:57 +0200

    Update jquery.timeago.sk.js
    Add full plural support

--- locales/jquery.timeago.sk.js
+++ locales/jquery.timeago.sk.js
@@ -1,18 +1,24 @@
 // Slovak
-jQuery.timeago.settings.strings = {
-  prefixAgo: "pred",
-  prefixFromNow: null,
-  suffixAgo: null,
-  suffixFromNow: null,
-  seconds: "menej než minútou",
-  minute: "minútou",
-  minutes: "%d minútami",
-  hour: "hodinou",
-  hours: "%d hodinami",
-  day: "1 dňom",
-  days: "%d dňami",
-  month: "1 mesiacom",
-  months: "%d mesiacmi",
-  year: "1 rokom",
-  years: "%d rokmi"
-};
+(function() {
+	function f(n, d, a) {
+		return a[d>=0 ? 0 : a.length===2 || n<5 ? 1 : 2];
+	}
+
+	jQuery.timeago.settings.strings = {
+		prefixAgo:     'pred',
+		prefixFromNow: 'o',
+		suffixAgo:     null,
+		suffixFromNow: null,
+		seconds: function(n, d) {return f(n, d, ['menej ako minútou', 'menej ako minútu']);},
+		minute:  function(n, d) {return f(n, d, ['minútou', 'minútu']);},
+		minutes: function(n, d) {return f(n, d, ['%d minútami', '%d minúty', '%d minút']);},
+		hour:    function(n, d) {return f(n, d, ['hodinou', 'hodinu']);},
+		hours:   function(n, d) {return f(n, d, ['%d hodinami', '%d hodiny', '%d hodín']);},
+		day:     function(n, d) {return f(n, d, ['%d dňom', '%d deň']);},
+		days:    function(n, d) {return f(n, d, ['%d dňami', '%d dni', '%d dní']);},
+		month:   function(n, d) {return f(n, d, ['%d mesiacom', '%d mesiac']);},
+		months:  function(n, d) {return f(n, d, ['%d mesiacmi', '%d mesiace', '%d mesiacov']);},
+		year:    function(n, d) {return f(n, d, ['%d rokom', '%d rok']);},
+		years:   function(n, d) {return f(n, d, ['%d rokmi', '%d roky', '%d rokov']);}
+	};
+})();
