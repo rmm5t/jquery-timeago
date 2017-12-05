0a20d4256cec072b50a54f6f03fdcebfe11b03cd
Serhan Apaydın <zerhan@gmail.com>
2017-12-05 18:38:16 +0300

    lint

--- locales/jquery.timeago.tr.js
+++ locales/jquery.timeago.tr.js
@@ -9,18 +9,18 @@
 }(function (jQuery) {
   // Turkish
   jQuery.timeago.settings.strings = {
-     suffixAgo: 'önce',
-     suffixFromNow: null,
-     seconds: '1 dakikadan',
-     minute: '1 dakika',
-     minutes: '%d dakika',
-     hour: '1 saat',
-     hours: '%d saat',
-     day: '1 gün',
-     days: '%d gün',
-     month: '1 ay',
-     months: '%d ay',
-     year: '1 yıl',
-     years: '%d yıl'
+    suffixAgo: 'önce',
+    suffixFromNow: null,
+    seconds: '1 dakikadan',
+    minute: '1 dakika',
+    minutes: '%d dakika',
+    hour: '1 saat',
+    hours: '%d saat',
+    day: '1 gün',
+    days: '%d gün',
+    month: '1 ay',
+    months: '%d ay',
+    year: '1 yıl',
+    years: '%d yıl'
   };
 }));
